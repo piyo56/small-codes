8c0c36347c244d6b20488199cb62d171a253a298
piyo56 <piyo56.net@gmail.com>
2016-09-07 14:29:52 +0900

    [Update] add title check

--- scraper.py
+++ scraper.py
@@ -6,12 +6,13 @@ r = requests.get('http://pyconjp.connpass.com/event/30692/')
 
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, "lxml")
-    print(soup.title())
-    target_div = soup.select(".ptype")
-    for e in target_div:
-        acceptance_num = e.select(".amount")[1].contents[1].split("/")[1]
-        reservation_num = e.select(".amount_over")[0].contents[0]
-        #print("{0}/{1}".format(reservation_num, acceptance_num))
+    title_name = "".join(soup.find("title").contents[0].split()[:3])
+    if title_name == "PyConJP2016":
+        target_div = soup.select(".ptype")
+        for e in target_div:
+            acceptance_num = e.select(".amount")[1].contents[1].split("/")[1]
+            reservation_num = e.select(".amount_over")[0].contents[0]
+            print("{0}/{1}".format(reservation_num, acceptance_num))
 else:
     #エラーの旨をhogehoge
     pass
