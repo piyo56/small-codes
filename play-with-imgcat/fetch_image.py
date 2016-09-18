590d395558166cc64f6d3191301e154959a0f5db
piyo56 <piyo56.net@gmail.com>
2016-09-18 21:29:27 +0900

    [Update](play-with-imgcat) エラー処理を追加（エラー用に透明画像を用意）

--- play-with-imgcat/fetch_image.py
+++ play-with-imgcat/fetch_image.py
@@ -8,8 +8,8 @@ import random
 # コマンドライン引数から検索ワードを取得（複数の場合はそこからランダムに選ぶ）
 if not (len(sys.argv) > 1):
     sys.stderr.write("usage: python fetch_image.py [search words]\n")
-    #with open("./error.png", "rb") as f:
-    #    sys.stdout.buffer.write(f.read())
+    with open("./error.png", "rb") as f:
+        sys.stdout.buffer.write(f.read())
     sys.exit(1)
 
 search_words = sys.argv[1:]
@@ -28,9 +28,9 @@ request_url = web_site + GET_args
 r = requests.get(request_url, stream='True')
 results = json.loads(r.text)
 hit_image_nums = len(results["data"])
-rand_num = random.randint(1, hit_image_nums)
-image_url = results["data"][rand_num]["images"]["fixed_height_downsampled"]["url"]
-#image_url = results["data"][rand_num]["images"]["fixed_height"]["url"]
+rand_num = random.randint(0, hit_image_nums-1)
+#image_url = results["data"][rand_num]["images"]["fixed_height_downsampled"]["url"]
+image_url = results["data"][rand_num]["images"]["fixed_height"]["url"]
 
 # gif画像を取得してバイナリを標準出力に流す
 sys.stderr.write(image_url + "\n")
