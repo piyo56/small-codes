0fb0fb0c25937183f2c2e830f2da829499e0015a
piyo56 <piyo56.net@gmail.com>
2016-09-20 21:45:44 +0900

    [Update](PyconJp) 例外をキャッチして表示するように改良

--- PyconJp/scraper.py
+++ PyconJp/scraper.py
@@ -1,31 +1,45 @@
+import sys
+import time
 from bs4 import BeautifulSoup
 import requests
 from pync import Notifier
 
-r = requests.get('http://pyconjp.connpass.com/event/30692/')
 
-fully_occupied = True
+# ページソースを取得してBeautifulSoupのオブジェクトを返す
+def fetch_page_source(url):
+    time.sleep(1)
+    target_page = requests.get(url)
+    if target_page.status_code == 200:
+        soup = BeautifulSoup(target_page.text, "lxml")
+        return soup
+    else:
+        print_error("failed to fetch page source (status code:{})".format(target_page.status_code))
+        return None
 
-# GET response status
-if r.status_code == 200:
-    soup = BeautifulSoup(r.text, "lxml")
-    target_div = soup.select(".ptype")
-    #title_name = "".join(soup.find("title").contents[0].split()[:3])
-    #if title_name == "PyConJP2016":
-    
-    # 応募種類から予約数と受け入れ数を取得
-    for e in target_div:
-        entry_type = e.select(".ptype_name")[0].contents[0]
-        acceptance_num = int(e.select(".amount")[1].contents[1].split("/")[1])
-        reservation_num = int(e.select(".amount_over")[0].contents[0])
-        #print("{0}/{1}".format(reservation_num, acceptance_num))
+if __name__ == "__main__":
+    soup = fetch_page_source('http://pyconjp.connpass.com/event/30692/')
 
-        if acceptance_num - reservation_num > 0:
-            fully_occupied = False
-            Notifier.notify("{0}の席に空きがでました!!".format(entry_type), title="PyConJP2016")
+    if soup is not None:
+        fully_occupied = True
+        target_div = soup.select(".ptype")
+        print(len(target_div))
 
-    if fully_occupied:
-        Notifier.notify("まだ満員です...", title="PyConJP2016")
+        # 応募種類から予約数と受け入れ数を取得
+        for e in target_div:
+            try:
+                entry_type      = e.select(".ptype_name")[0].contents[0]
+                acceptance_num  = int(e.select(".amount")[1].contents[1].split("/")[1])
+                reservation_num = int(e.select(".amount_over")[0].contents[0])
+                print("{}:{}/{}".format(entry_type, reservation_num, acceptance_num))
+                # 空きがあれば通知
+                if acceptance_num - reservation_num > 0:
+                    fully_occupied = False
+                    Notifier.notify("{0}の席に空きがでました!!".format(entry_type), title="PyConJP2016")
+            except Exception as e:
+                print("({} ---> {})".format(entry_type, e.args[0]))
+                continue
 
-else:
-    Notifier.notify("PyConJP2016のページソースの取得に失敗しました", title="PyConJP2016")
+        if fully_occupied:
+            Notifier.notify("まだ満員です...", title="PyConJP2016")
+    else:
+        Notifier.notify("PyConJP2016のページソースの取得に失敗しました", title="PyConJP2016")
