c128b1a3d244e9110eea629f6aaf361109225a5d
piyo56 <piyo56.net@gmail.com>
2017-01-20 21:03:54 +0900

    add the most simple react example

--- zozotown/notify_restock.py
+++ zozotown/notify_restock.py
@@ -10,8 +10,10 @@ from pync import Notifier
     最近は再入荷ボタンが出現しない傾向にあるので困っている
 """
 
-# ページソースを取得してBeautifulSoupのオブジェクトを返す
 def fetch_page(url):
+    """
+    ページソースを取得してBeautifulSoupのオブジェクトを返す
+    """
     time.sleep(1)
     target_page = requests.get(url)
     enc = target_page.encoding
@@ -19,16 +21,16 @@ def fetch_page(url):
         soup = BeautifulSoup(target_page.text.encode(enc), "lxml")
         return soup
     else:
-        print_error("failed to fetch page source (status code:{})".format(target_page.status_code))
+        print("failed to fetch page source (status code:{})".format(target_page.status_code))
         return None
 
 if __name__ == "__main__":
-    target_url = "http://zozo.jp/shop/journalstandard/goods/12677141/?rid=1019"
+    target_url = "http://zozo.jp/shop/abahouse/goods-sale/14802339/?did=32762540&rid=1095"
     soup = fetch_page(target_url)
 
     if soup is not None:
         cart_div = soup.select(".cart")
         
         # 今回のアイテムの場合、カラー3色のサイズ3種で合計9つのdivがヒット
-        if not cart_div[0].text.strip() == "完売しました":
+        if not cart_div[5].text.strip() == "完売しました":
             Notifier.notify("再入荷しました！！", title="ZOZO", open=target_url, sound='Ping')
